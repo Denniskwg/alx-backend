3b6357fd4371a1ba52327f890a7482acde5829d6
Denniskwg <dkamau476@gmail.com>
2023-07-31 15:51:56 +0300

    adding solution for question 5

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""3-lru_cache
+"""4-mru_cache
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """LRUCache inherits from BaseCaching
+class MRUCache(BaseCaching):
+    """MRUCache inherits from BaseCaching
     """
     def __init__(self):
         """initializes an object from the class
@@ -18,7 +18,7 @@ class LRUCache(BaseCaching):
         item value for the key key
         If the number of items in self.cache_data is higher than
         BaseCaching.MAX_ITEMS:
-        you must discard the least recently used put in cache (LIFO algorithm)
+        you must discard the most recently usedin cache (MRU algorithm)
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
@@ -29,11 +29,11 @@ class LRUCache(BaseCaching):
             else:
                 self.track = [key] + self.track
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            val = self.track[BaseCaching.MAX_ITEMS]
-            del self.cache_data[val]
-            del self.track[BaseCaching.MAX_ITEMS]
-            print("DISCARD: {}".format(val))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                val = self.track[1]
+                del self.cache_data[val]
+                del self.track[1]
+                print("DISCARD: {}".format(val))
 
     def get(self, key):
         """return the value in self.cache_data linked to key
